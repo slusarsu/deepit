34e0be46eb72d94d458decdbb55f5f00bd794cd9
Vitalii Sliusar <slusarsu@gmail.com>
2023-07-18 00:55:08 +0300

    added comments

--- app/Filament/Resources/CommentResource.php
+++ app/Filament/Resources/CommentResource.php
@@ -87,6 +87,7 @@ class CommentResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
