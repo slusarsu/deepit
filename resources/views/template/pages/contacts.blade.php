8383b53b962a7d771c1a6467ef36181db920a3f9
Vitalii Sliusar <slusarsu@gmail.com>
2023-07-25 08:58:07 +0300

    try fix contact button

--- resources/views/template/pages/contacts.blade.php
+++ resources/views/template/pages/contacts.blade.php
@@ -73,8 +73,7 @@
                             data-callback='onSubmit'
                             data-action='submit'
                             type="submit"
-{{--                            value="{{$cf['form_button_text'] ?? ''}}"--}}
-                            value="{{$cf['form_button_text']}}"
+                            value="{{$cf['form_button_text'] ?? ''}}"
                         >
                     @else
                         <button type="submit" class="btn btn-primary">
